017b533700449db8aa75220adde92f0d8be8ecd3
Katlin2000 <katlin.pala@gmail.com>
2025-08-20 20:45:01 +0100

    added photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,31 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState("sunset");
+export default function Dictionary(props) {
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data);
+  }
+
+  function handlePicturesResponse(response) {
+    setPhotos(response.data.photos);
+    console.log(response.data.photos);
   }
 
   function search() {
     let apiKey = "e5bt54135b02f36048218oa4bf5cb2d1";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let photosApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}`;
+    axios.get(photosApiUrl).then(handlePicturesResponse);
   }
 
   function handleSubmit(event) {
@@ -37,10 +46,11 @@ export default function Dictionary() {
     return (
       <div className="Dictionary">
         <section>
+          <h2 className="title">What word do you want to look up?</h2>
           <form onSubmit={handleSubmit}>
             <input
               type="search"
-              placeholder="Sunset"
+              placeholder="Type a word..."
               onChange={handleKeywordChange}
             />
           </form>
@@ -48,6 +58,7 @@ export default function Dictionary() {
         </section>
 
         {results && <Results results={results} />}
+        <Photos photos={photos} />
       </div>
     );
   } else {
